5b2d5f844abc329e845de38fa312897b470bf192
Bhupendra <bhupendrabc71@gmail.com>
2023-12-26 18:46:30 +1030

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// JavaScript animation (simple scale animation)
+const cartoonImage = document.querySelector('.cartoon-image');
+
+function scaleAnimation() {
+    cartoonImage.style.transition = 'transform 0.5s';
+    cartoonImage.style.transform = 'scale(1.2)';
+}
+
+function resetAnimation() {
+    cartoonImage.style.transform = 'scale(1)';
+}
+
+cartoonImage.addEventListener('mouseover', scaleAnimation);
+cartoonImage.addEventListener('mouseout', resetAnimation);
